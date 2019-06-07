e42c3541495f3a45c1050bb84708cad38ed8787c
dhilst <danielhilst@gmail.com>
2019-06-07 02:08:00 -0300

    Traverse directories recursivelly

--- app/Main.hs
+++ app/Main.hs
@@ -4,49 +4,58 @@ import Lib
 import Text.Regex.Posix
 import System.Environment
 import Options.Applicative
+import Data.List
 import Data.Semigroup ((<>))
+import System.Directory
+import System.Directory.PathWalk
+import Control.Monad
+import System.FilePath.Posix
 
 import Utils
 
 data HrepOptions = HrepOptions
   { pattern         :: Regex 
-  , path            :: [FilePath] 
-  , fileSearchRegex :: Maybe Regex }
+  , path            :: [FilePath] }
 
-
-data Match = Match
+data HrepMatch = HrepMatch
   { text :: String
   , line :: Int
   , column :: Int
   , filePath :: String }
 
+instance Show HrepMatch where
+  show (HrepMatch t l c f) = intercalate ":" [f, (show l), (show c), t]
 
 hrepParser :: Parser HrepOptions
 hrepParser = HrepOptions
   <$> argument regexOpt (metavar "PATTERN")
   <*> many (argument str (metavar "PATH..."))
-  <*> option maybeRegexOpt 
-    (short 'G' 
-    <> long "file-search-regex" 
-    <> metavar "PATTERN"
-    <> help "Only search file whose name match PATTERN"
-    <> value Nothing)
 
-explodePaths :: [FilePath] Regex -> [FilePath]
-explodePaths path "" = path
+explodePaths :: [FilePath] -> Maybe Regex -> IO [FilePath]
+explodePaths path _ = return path
   
 hrep :: HrepOptions -> IO ()
-hrep (HrepOptions pattern [] Nothing) = hrepFile pattern "-"
-hrep (HrepOptions pattern paths Nothing) = mapM_ (hrepFile pattern) (explodePaths paths "")
-hrep (HrepOptions pattern paths (Just filePattern)) = mapM_ (hrepFile pattern) (explodePaths paths filePattern))
+hrep (HrepOptions pattern paths) = do 
+  forM_ paths $ \path -> do
+    isFile <- doesFileExist path
+    if isFile
+      then hrepFile pattern path
+      else pathWalk path $ \dir subdirs files -> do
+        forM_ files $ \file -> do
+          hrepFile pattern $ joinPath [dir, file]
+          hrep $ HrepOptions pattern $ map (\s -> joinPath [dir, s]) subdirs
 
 hrepFile :: Regex -> FilePath -> IO ()
 hrepFile pattern fileName = do
-  contents <- if fileName == "-" then getContents else readFile fileName
-  mapM_ printMatch $ filter (match pattern) (lines contents)
+  contents <- readFile fileName
+  forM_ (zip (lines contents) [1..]) $ \(line,lineNo) -> do
+    case matchOnce pattern line of
+      Nothing      -> return ()
+      Just matches -> forM_ matches $ \(offset,_) -> do
+        printMatch $ HrepMatch {text=line, line=lineNo, column=(offset+1), filePath=fileName}
 
-printMatch :: String -> IO ()
-printMatch match = putStrLn match
+printMatch :: HrepMatch -> IO ()
+printMatch match = putStrLn $ show match
 
 main :: IO ()
 main = hrep =<< execParser opts
--- app/Utils.hs
+++ app/Utils.hs
@@ -1,19 +1,10 @@
 module Utils 
   ( regexOpt
-  , maybeRegexOpt
   )
 where
 
 import Text.Regex.Posix
 import Options.Applicative
-
-eitherRegex :: String -> Either String (Maybe Regex)
-eitherRegex pattern
-  | pattern == "" = Left  $ "No pattern provided"
-  | otherwise     = Right $ Just (makeRegex pattern)
-
-maybeRegexOpt :: ReadM (Maybe Regex)
-maybeRegexOpt = eitherReader eitherRegex
 
 regexOpt :: ReadM Regex
 regexOpt = eitherReader $ Right . makeRegex
